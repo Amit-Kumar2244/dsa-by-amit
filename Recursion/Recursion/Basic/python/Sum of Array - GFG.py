3ca8fe9a191399c14c282d11eeeaf3405feadd15
Amit-Kumar2244 <amitkumarnagvanshi2244@gmail.com>
2025-07-04 08:56:27 +0530

    Delete Recursion directory

--- Recursion/Recursion/Basic/python/Sum of Array - GFG.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def sum(self, arr, n):
-        if n <= 0:
-            return 0
-        return arr[n - 1] + self.sum(arr, n - 1)
-
-    def arraySum(self, arr):
-        n = len(arr)
-        return self.sum(arr, n)
