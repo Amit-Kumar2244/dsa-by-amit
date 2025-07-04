1158f3bfa88f85c0c8032914d1a909c1b9abcdb8
Amit-Kumar2244 <amitkumarnagvanshi2244@gmail.com>
2025-07-04 12:37:15 +0530

    Delete Linked List/Python directory

--- Linked List/Python/GFG - PRINT LINKED LIST.py
+++
@@ -1,9 +0,0 @@
-# GFG - Print Linked List
-# QUESTION LINK - https://www.geeksforgeeks.org/problems/print-linked-list-elements/1
-
-class Solution:
-    def printList(self, head):
-        temp = head
-        while temp is not None:
-            print(temp.data, end=" ")
-            temp = temp.next
